fc04474c408764ec04f4f476f4ee33223a0166d7
Haku1993 <frjrodriguezla@unal.edu.co>
2018-11-26 20:37:10 -0500

    Correcion_del_programa_inicial

--- 1.00.cpp
+++ 1.00.cpp
@@ -1,4 +1,74 @@
 
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+// global constants
+const int N = 1;
+const double G = 9.81; // m/s^2
+const double DT = 0.01; // m/s^2
+const double K = 300.234; // N/m
+
+// body class
+struct Body {
+  double Rxold = 0, Ryold = 0, Rx = 0, Ry = 0, Vx = 0, Vy = 0, Fx = 0, Fy = 0;
+  double mass = 0;
+  double rad = 0;
+  void arranque(double dt);
+  void timestep(double dt);
+};
+
+void Body::arranque(double dt)
+{
+  Rxold = Rx - dt*Vx + Fx*dt*dt/(2*mass);   
+  Ryold = Ry - dt*Vy + Fy*dt*dt/(2*mass);   
+}
+
+void Body::timestep(double dt)
+{
+  double tmp;
+
+  tmp = Rx;
+  Rx = 2*Rx - Rxold + Fx*dt*dt/(mass);   
+  Vx = (Rx - Rxold)/(2*dt);
+  Rxold = tmp;
+
+  tmp = Ry;
+  Ry = 2*Ry - Ryold + Fy*dt*dt/(mass);   
+  Vy = (Ry - Ryold)/(2*dt);
+  Ryold = tmp;
+}
+
+// function declarations
+void set_masses(Body bodies[]);
+
+void compute_forces(Body bodies[]);
+void start(Body bodies[], double dt);
+void evolve(Body bodies[], double dt);
+
+void init_gnuplot(void);
+void print_to_gnuplot(Body bodies[]);
+
+// main
+int main(void)
+{
+  srand(0); //random seed
+
+  std::ofstream fout("datos.txt");
+
+  Body bodies[N];
+
+  bodies[0].rad = 0.45;
+  bodies[0].Rx = 0;
+  bodies[0].Ry = 4.545;
+  bodies[0].Vx = 0.0;
+  set_masses(bodies);
+  compute_forces(bodies);
+
+  init_gnuplot();
+
+  start(bodies, DT);
+
   for (int it = 0; it < 2000; ++it) {
     fout << DT*it << "  " << bodies[0].Ry << std::endl; 
     compute_forces(bodies);
